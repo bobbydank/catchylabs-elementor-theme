20763bc451025446982f5c2a69fa596d87ffd787
Robert Danklefsen <robert.danklefsen@barn3s.com>
2022-05-18 22:14:20 -0500

    Moving shit around

---
+++ admin/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => 'catchylabs/cl-elementor',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'catchylabs/cl-elementor' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+        'ignitekit/wp-option-builder' => array(
+            'pretty_version' => '1.0.4',
+            'version' => '1.0.4.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../ignitekit/wp-option-builder',
+            'aliases' => array(),
+            'reference' => 'cb2ccd64df46f23db8f6cf23596ade160d151117',
+            'dev_requirement' => false,
+        ),
+    ),
+);
